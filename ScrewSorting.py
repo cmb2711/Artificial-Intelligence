f56d9ce5a32b67ade8e1c9d95ccbe067a4d3b839
cmb2711 <45180163+cmb2711@users.noreply.github.com>
2023-09-07 10:46:51 -0400

    Formated ScrewSorting Correctly

--- ScrewSorting.py
+++ ScrewSorting.py
@@ -6,9 +6,11 @@ class Screw:
         return("(" + self.Name + ", " + str(self.Weight) + ")")
     def __repr__(self):
         return("(" + self.Name + ", " + str(self.Weight) + ")")
+    
 class ScrewList:
     def __init__(self):
         self.arr = []
+
     def add(self, Screw):
         self.arr.append(Screw)
         self.insertion_sort()
@@ -18,45 +20,37 @@ class ScrewList:
         print("Placement Index: " + str(index))
         print("Placed Between " + str(self.get(index - 1)) + " and " + str(self.get(index + 1)))
         print("_______________________________________________________________________________")
+
     def get(self, index):
         if index<0 or index >= len(self.arr):
             return None
         return self.arr[index]
+    
     def binarysearch(self, arr, low, high, x):
         try:
             x = x.Weight
         except:
             pass
-        # Check base case
         if high >= low:
-    
             mid = (high + low) // 2
-    
-            # If element is present at the middle itself
             if arr[mid].Weight == x:
                 return mid
-    
-            # If element is smaller than mid, then it can only
-            # be present in left subarray
             elif arr[mid].Weight > x:
                 return self.binarysearch(arr, low, mid - 1, x)
-    
-            # Else the element can only be present in right subarray
             else:
                 return self.binarysearch(arr, mid + 1, high, x)
-    
         else:
-            # Element is not present in the array
             return -1
-            
   
     def insertion_sort(self):
         i = len(self.arr) - 1
         val = self.arr[i]
         j = self.binarysearch(self.arr, 0, i-1, val)
         self.arr = self.arr[:j] + [val] + self.arr[j:i] + self.arr[i+1:]
+        
     def __str__(self):
         return str(self.arr)
+    
 Screws= ScrewList()
 while True:
     Screws.add(Screw(input("Name: "), float(input("Weight: "))))
